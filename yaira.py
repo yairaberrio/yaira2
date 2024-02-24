9468fc900f26e1780dcb25dae09294ad9312be32
yairaberrio <161092887+yairaberrio@users.noreply.github.com>
2024-02-23 19:35:24 -0500

    Add files via upload

---
+++ yaira.py
@@ -0,0 +1,13 @@
+num1 = 10
+num2 = 20
+
+suma = num1 + num2
+resta = num1 - num2
+multiplicación = num1 * num2
+division = num1 / num2
+
+print(suma)
+print(resta)
+print(multiplicación)
+print(division)
+
